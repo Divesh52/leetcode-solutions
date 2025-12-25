0db8766942de23a2ac5e1cfef12756afa01bf802
Divesh52 <diveshkoul5@gmail.com>
2025-12-25 13:27:16 +0530

    Create centre_of_star_graph.cpp

---
+++ graphs/centre_of_star_graph.cpp
@@ -0,0 +1,11 @@
+// Problem 1791: Find Center of Star Graph
+// Approach: Graph / Direct Comparison
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        if(edges[0][0]==edges[1][0]||edges[0][0]==edges[1][1]) return edges[0][0];
+        return edges[0][1];
+    }
+};
