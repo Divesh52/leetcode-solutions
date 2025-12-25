b9923cdc700062a632f77ce02e7583741518c118
Divesh52 <diveshkoul5@gmail.com>
2025-12-25 13:36:35 +0530

    Create sliding_window_maximum.cpp

---
+++ queue/sliding_window_maximum.cpp
@@ -0,0 +1,20 @@
+// Problem 239: Sliding Window Maximum
+// Approach: Queue / Deque
+// Time Complexity: O(n)  (each element pushed and popped at most once)
+// Space Complexity: O(k)  (for deque)
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> dq;
+
+        for (int i = 0; i < nums.size(); i++) {
+            while (!dq.empty() && dq.front() <= i - k) dq.pop_front();
+            while (!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
+            dq.push_back(i);
+            if (i >= k - 1) ans.push_back(nums[dq.front()]);
+        }
+
+        return ans;
+    }
+};
