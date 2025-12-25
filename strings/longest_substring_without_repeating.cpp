6ec7615658e80fbee55e63dbcfaefe3fdc375a52
Divesh52 <diveshkoul5@gmail.com>
2025-12-25 12:07:44 +0530

    Create longest_substring_without_repeating.cpp

---
+++ strings/longest_substring_without_repeating.cpp
@@ -0,0 +1,31 @@
+// Problem 3: Longest Substring Without Repeating Characters
+// Approach: Brute Force / Hash Set
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
+class Solution {
+public:
+    bool f(const string& str) {
+    unordered_set<char> chars;
+    for (char c : str) {
+        if (chars.count(c)) return false;
+        chars.insert(c);
+    }
+    return true;
+}
+    int lengthOfLongestSubstring(string s) {
+        if(s=="") return 0;
+        if (s == "") return 0;
+        int m = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            unordered_set<char> c;
+            for (int j = i; j < s.size(); j++) {
+                if (c.count(s[j])) break;
+                c.insert(s[j]);
+                m = max(m, j - i + 1);
+            }
+        }
+
+        return m;
+    }
+};
