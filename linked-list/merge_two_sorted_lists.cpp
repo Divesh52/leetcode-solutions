40a281d41db73a2eac711b68764316c7b190fd18
Divesh52 <diveshkoul5@gmail.com>
2025-12-25 12:44:28 +0530

    Create merge_two_sorted_lists.cpp

---
+++ linked-list/merge_two_sorted_lists.cpp
@@ -0,0 +1,27 @@
+// Problem 21: Merge Two Sorted Lists
+// Approach: Linked List / Recursion
+// Time Complexity: O(m + n)
+// Space Complexity: O(m + n)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(!list1||!list2) 
+        {
+            if (list1) return list1;
+            else return list2;
+        }
+        if(list1->val>list2->val) swap(list1,list2);
+        list1->next=mergeTwoLists(list1->next,list2);
+        return list1;
+    }
+};
