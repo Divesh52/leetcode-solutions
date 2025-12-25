8202682ab94e8cc770d3a856d3d694825f74c019
Divesh52 <diveshkoul5@gmail.com>
2025-12-25 12:02:42 +0530

    Update search_in_sorted_array.cpp

--- arrays/search_in_sorted_array.cpp
+++ arrays/search_in_sorted_array.cpp
@@ -3,9 +3,6 @@
 // Time Complexity: O(log n)
 // Space Complexity: O(1)
 
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -13,14 +10,27 @@ public:
 
         while(left <= right) {
             int mid = left + (right - left) / 2;
+
             if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
+
+            if(nums[left] <= nums[mid]) {
+                if(nums[left] <= target && target < nums[mid]) 
+                    right = mid - 1;
+                else 
+                    left = mid + 1;
+            } else {
+                if(nums[mid] < target && target <= nums[right]) 
+                    left = mid + 1;
+                else 
+                    right = mid - 1;
+            }
         }
 
         return -1;
     }
 };
+
+//Second approach
 // Problem: Search in Sorted Array
 // Approach: Binary Search
 // Time Complexity: O(n)
